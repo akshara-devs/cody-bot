c0e8d3f65a04cf0b8da5ad79a978655835fb2364
Mogu <riskymuhamad352@gmail.com>
2026-03-22 16:52:18 +0700

    setup:first testing

---
+++ cogs/project.py
@@ -0,0 +1,15 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Project(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("Pong! 🏓")
+    
+
+async def setup(bot):
+    await bot.add_cog(Project(bot))
---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN_BOT")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Login sebagai {bot.user}")
+
+async def main():
+    async with bot:
+        await bot.load_extension("cogs.project")
+        await bot.load_extension("cogs.coin")
+        await bot.start(TOKEN)
+
+asyncio.run(main())
